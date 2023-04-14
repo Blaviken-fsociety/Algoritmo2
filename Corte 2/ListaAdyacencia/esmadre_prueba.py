c5a17dec92a460a7e5f5033e78f81b40bf84bf84
Blaviken <jarellan@cuc.edu.co>
2023-04-14 15:36:34 -0500

    14/04/2023

--- Corte 2/ListaAdyacencia/esmadre_prueba.py
+++ Corte 2/ListaAdyacencia/esmadre_prueba.py
@@ -12,10 +12,14 @@ grafo.adicionarVertice(5)
 
 # Agregar aristas
 grafo.adicionarArco(1, 2)
+grafo.adicionarArco(2, 1)
 grafo.adicionarArco(2, 3)
 grafo.adicionarArco(3, 4)
 grafo.adicionarArco(4, 5)
 print("Grafo: ",grafo)
 
 # Verificar si existe un camino desde el vertice 1 hacia todos los demas vertices del grafo
-print(grafo.esMadre(1))
+print(grafo.esMadre(2))#True
+print(grafo.esMadre(1))#True
+print(grafo.esMadre(4))#False
+print(grafo.esMadre(6))#None
--- Corte 2/ListaAdyacencia/grafoEntrada_prueba.py
+++ Corte 2/ListaAdyacencia/grafoEntrada_prueba.py
@@ -11,12 +11,18 @@ grafo.adicionarVertice("C")
 
 # Agregamos algunos arcos
 grafo.adicionarArco("A", "B")
+grafo.adicionarArco("C", "B")
 grafo.adicionarArco("B", "C")
 grafo.adicionarArco("C", "A")
 
 print("Grafo: ",grafo)
 
 # Probamos la función gradoEntrada para el nodo "C"
-grado_entrada = grafo.gradoEntrada("C")
+grado_entrada = grafo.gradoEntrada("C")#1
 print(grado_entrada)
 # Debería imprimir 1, ya que hay un arco entrando al nodo "C"
+
+# Probamos la función gradoEntrada para el nodo "B"
+grado_entrada = grafo.gradoEntrada("B")#2
+print(grado_entrada)
+# Debería imprimir 2, ya que hay un arco entrando al nodo "B"
