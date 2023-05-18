e8bc9fb5e8e8a394abb62f69171636f0e673a5c9
blaviken-fsociety <jarellan@cuc.edu.co>
2023-05-17 20:29:27 -0500

    17/05/2023

--- Corte 3/Taller #3/obtenerFracciones_prueba.py
+++ Corte 3/Taller #3/obtenerFracciones_prueba.py
@@ -4,5 +4,3 @@ arbol = ArbolBinarioBusqueda()
 lista_fracciones = [Fraccion(1, 2), Fraccion(3, 4), Fraccion(1, 6), Fraccion(1, 3), Fraccion(3, 2), Fraccion(4, 5), Fraccion(7, 4)]
 arbol.insertarNodosListaFraccionarios(lista_fracciones)
 print(arbol)
-
-
